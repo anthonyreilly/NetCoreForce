7c3c2ad14d5ca0e3bbcde36fdc2bf540f21b792e
Anthony Reilly <anthony@areilly.com>
2018-03-02 11:04:14 -0500

    HeaderFormatter now returns single-entry Dictionary instead of KeyValuePair. Query options formatter throws ArgumentException if batchSize is outside 200-2000.

--- src/NetCoreForce.Client.Tests/HeaderFormatterTest.cs
+++ src/NetCoreForce.Client.Tests/HeaderFormatterTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using NetCoreForce.Client;
 using Xunit;
 
@@ -12,9 +13,13 @@ namespace NetCoreForce.Client.Tests
         {
             DateTimeOffset date = new DateTimeOffset(2015, 3, 23, 0, 0, 0, 0, new TimeSpan(0));
 
-            KeyValuePair<string, string> header = HeaderFormatter.IfModifiedSince(date);
+            Dictionary<string, string> customHeaders = HeaderFormatter.IfModifiedSince(date);
 
-            string result = string.Format("{0}: {1}", header.Key, header.Value);            
+            Assert.Equal(1, customHeaders.Count);
+
+            var header = customHeaders.First();
+
+            string result = string.Format("{0}: {1}", header.Key, header.Value);
 
             Assert.Equal("If-Modified-Since: Mon, 23 Mar 2015 00:00:00 GMT", result);
         }
@@ -22,19 +27,45 @@ namespace NetCoreForce.Client.Tests
         [Fact]
         public void QueryOptionsHeader()
         {
-            KeyValuePair<string, string> header = HeaderFormatter.SforceQueryOptions(1000);
+            Dictionary<string, string> customHeaders = HeaderFormatter.SforceQueryOptions(1000);
 
-            string result = string.Format("{0}: {1}", header.Key, header.Value);            
+            Assert.Equal(1, customHeaders.Count);
+
+            var header = customHeaders.First();
+
+            string result = string.Format("{0}: {1}", header.Key, header.Value);
 
             Assert.Equal("Sforce-Query-Options: batchSize=1000", result);
         }
 
         [Fact]
+        public void QueryOptionsHeaderMaxException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var customHeaders = HeaderFormatter.SforceQueryOptions(2001);
+            });
+        }
+
+        [Fact]
+        public void QueryOptionsHeaderMinException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var customHeaders = HeaderFormatter.SforceQueryOptions(0);
+            });
+        }
+
+        [Fact]
         public void AutoAssignHeader()
         {
-            KeyValuePair<string, string> header = HeaderFormatter.SforceAutoAssign(false);
+            Dictionary<string, string> customHeaders = HeaderFormatter.SforceAutoAssign(false);
 
-            string result = string.Format("{0}: {1}", header.Key, header.Value);            
+            Assert.Equal(1, customHeaders.Count);
+
+            var header = customHeaders.First();
+
+            string result = string.Format("{0}: {1}", header.Key, header.Value);
 
             Assert.Equal("Sforce-Auto-Assign: FALSE", result);
         }
