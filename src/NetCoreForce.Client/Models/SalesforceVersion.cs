d893afd7ba4deb96289bdeb33adf1c6234e453ee
Anthony Reilly <anthony@areilly.com>
2018-11-19 13:01:36 -0500

    Removed SalesforceVersions model object, since it did not match array of versions objects returned by the Versions resource.

--- src/NetCoreForce.Client/Models/SalesforceVersion.cs
+++ src/NetCoreForce.Client/Models/SalesforceVersion.cs
@@ -2,21 +2,30 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace NetCoreForce.Client.Models
-{
-    public class SalesforceVersions
-    {
-        [JsonProperty(PropertyName = "versions")]
-        public List<SalesforceVersion> Versions { get; set; }
-    }
-    
+{    
+    /// <summary>
+    /// API Version, returned by the Versions resource
+    /// </summary>
     public class SalesforceVersion
     {
+        /// <summary>
+        /// API Version, e.g. "44.0"
+        /// </summary>
+        /// <value></value>
         [JsonProperty(PropertyName = "version")]
         public string Version { get; set; }
 
+        /// <summary>
+        /// Base URL of the version's root, e.g. "/services/data/v44.0"
+        /// </summary>
+        /// <value></value>
         [JsonProperty(PropertyName = "url")]
         public string Url { get; set; }
 
+        /// <summary>
+        /// Name of the version, e.g. "Winter '19"
+        /// </summary>
+        /// <value></value>
         [JsonProperty(PropertyName = "label")]
         public string Label { get; set; }
     }
